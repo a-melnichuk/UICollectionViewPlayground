924ab5632fd6b79f32f9eff311e253a515e40828
Alex Melnichuk <melnichuk.al2@gmail.com>
2017-10-24 19:47:10 +0300

    Initial Commit

---
+++ UIPlayground/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UIPlayground
+//
+//  Created by Alex Melnichuk on 10/24/17.
+//  Copyright © 2017 Alex Melnichuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
